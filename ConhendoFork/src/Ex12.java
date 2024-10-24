4a5a4261e7a5d5280b50f9977a00e3da5f4a7d1d
Diogo <0001085533@senaimgaluno.com.br>
2024-10-24 15:15:02 -0300

    adicionando arquivo Ex12.java

--- ConhendoFork/src/Ex12.java
+++ ConhendoFork/src/Ex12.java
@@ -1,11 +1,37 @@
 /*Verificação de Números Primos: Leia números do usuário até que ele insira 
  * um número negativo e informe se cada número é primo ou não.
  */
+//Desenvolvido por Diogo e Giovanna.
+
+import java.util.Scanner;
 public class Ex12 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		
+		boolean primo = true;
+		int numero;
+		
+		while(true) {
+			System.out.println("Digite um número: ");
+			numero = scanner.nextInt();
+			
+			if (numero <= 1) {
+				primo = false;
+			}else {
+				for(int i = 2; i < numero; i++) {
+					if(numero % i == 0) {
+						primo = false;
+						break;
+					}
+				}
+			}
+			if(primo) {
+				System.out.println("O " + numero + " é primo");
+			}else {
+				primo = true;
+				System.out.println("O " + numero + " não é primo");
+			}
+		}	
 	}
-
 }
